929de3bc14bb4e4f5b7ce4bf1ef92855c00a98d6
Jonay Faas Garcia <alu0101470948@ull.edu.es>
2023-01-22 03:00:30 +0000

    Version 1.01
    shebang line added, minor fixes

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
+#!/usr/bin/python3
+
 import random
 import string
 from subprocess import Popen,PIPE
 
-def password_generator(lenght,spc_chars=False):
+def password_generator(length,spc_chars=False):
     if spc_chars == True:
         characters = string.ascii_letters + string.digits + string.punctuation
     else:
         characters = string.ascii_letters + string.digits
-    password = "".join(random.choice(characters) for i in range(lenght))
+    password = "".join(random.choice(characters) for i in range(length))
     return password
 
-def out(password,lenght):
-    for i in range(lenght+4):
+def out(password,length):
+    for i in range(length+4):
         print("*", end="")
     print("\n* " + password + " *")
-    for i in range(lenght+4):
+    for i in range(length+4):
         print("*", end="")
     print("")
 
@@ -22,16 +24,16 @@ def copy_clipboard(msg):
     with Popen(['xclip','-selection', 'clipboard'], stdin=PIPE) as pipe:
         pipe.communicate(input=msg.encode('utf-8'))
 
-lenght = int(input("Insert password lenght: "))
+length = int(input("Insert password length: "))
 special_characters = input("Want to add special characters? (Y/n) ")
-acepted_affirmative = {"Y","yes","y"}
+acepted_affirmative = {"Y","y","Yes","yes"}
 
 if special_characters in acepted_affirmative:
-    password = password_generator(lenght,True)
+    password = password_generator(length,True)
 else:
-    password = password_generator(lenght)
+    password = password_generator(length)
 
-out(password,lenght)
+out(password,length)
 
 clipboard = input("Add password to clipboard? (Y/n) ")
 if clipboard in acepted_affirmative:
